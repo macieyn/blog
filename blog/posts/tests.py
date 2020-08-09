e39eff54d5ae2e30aef289433407d2b43ab0db18
macieyn <maciej.nachtygal@gmail.com>
2020-08-09 18:28:47 +0200

    url resolve to posts

--- blog/posts/tests.py
+++ blog/posts/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.urls import resolve
+
+from . import views
 
 # Create your tests here.
+
+class PostPageTests(TestCase):
+
+    def test_resolve(self):
+        response = resolve('/')
+        self.assertEqual(views.posts, response.func)
+
+    def test_return_http_re
